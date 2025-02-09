1f5a225108bd18c03e3ea1efe89fde2db1a56a8b
Elvis <heme1120242@gmail.com>
2025-02-09 17:50:35 +0100

    last one

--- Script.js
+++ Script.js
@@ -1,12 +1,12 @@
 // Oppgave 2: Variabler med ulike datatyper
 
-// Vi bruker let for variabler som kan endres, og const for konstanter.
+// We use let for variables that can be changed, and const for constants.
 let myString = "Hello World!"; // String (text)
 const myNumber = 32; // Number )
 let myBoolean = true; // Boolean (True/Untrue)
 const myArray = [1, 2, 3, 4, 5]; // Array (list)
 
-// Skriv ut variablene til konsollen for å se innholdet:
+// Print the variables to the console to see their contents:
 console.log(myString);
 console.log(myNumber);
 console.log(myBoolean);
@@ -16,7 +16,7 @@ console.log(myArray);
 /* - Matematiske operatorer: +, -, /, *
 - Forkortede operatorer: ++, --, +=, -= */
 
-//Matematiske operatorer
+//Mathematical operators
 let a = 15;
 let b = 20;
 console.log(a + b);
@@ -24,7 +24,7 @@ console.log(a - b);
 console.log(a / b);
 console.log(a * b);
 
-// Forkortede operatorer:
+// Abbreviated operators:
 a++; // a = a + 1 (Increases with 1, now a is 16)
 console.log(a);
 
